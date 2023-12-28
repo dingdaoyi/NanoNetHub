67b9cdafb7634963c3de415f18010b8f44f7b9f0
岩冰 <1493001032@qq.com>
2023-12-28 13:28:08 +0800

    大概重构了下代码

--- nano_net_hub/src/lib.rs
+++ nano_net_hub/src/lib.rs
@@ -1,5 +1,7 @@
 use std::collections::HashMap;
 use std::env;
+use std::fs::File;
+use std::io::Read;
 
 use crate::config::database::DataSourceType;
 use crate::models::ServerError;
@@ -7,6 +9,7 @@ use crate::protocol::config_protocol;
 use crate::server::{Server, ServerConfig};
 use sqlx::sqlite::SqliteRow;
 use sqlx::{Pool, Sqlite};
+use toml::Value;
 
 mod config;
 pub mod models;
@@ -28,11 +31,25 @@ pub async fn run_server() -> Result<(), ServerError> {
 }
 
 async fn start_protocol_driver() -> Result<(), ServerError> {
+    let mut file = File::open("config.toml").expect("Unable to open the file");
+    let mut toml_str = String::new();
+    file.read_to_string(&mut toml_str)
+        .expect("Unable to read the file");
+    // 解析配置文件内容
+    let toml_value: Value = toml::from_str(&toml_str).expect("Unable to parse TOML");
+    let host = toml_value["mqtt"]["host"].as_str().unwrap();
+    let port = toml_value["mqtt"]["port"].as_str().unwrap();
+    let username = toml_value["mqtt"]["username"].as_str();
+    let password = toml_value["mqtt"]["password"].as_str();
     let mut config = HashMap::new();
-    config.insert("host", "mqtt.diweiyunlian.cn");
-    config.insert("port", "2840");
-    config.insert("username", "xiaohuoshuan");
-    config.insert("password", "xiaohuoshuan");
+    config.insert("host", host);
+    config.insert("port", port);
+    if username.is_some() {
+        config.insert("username", username.unwrap());
+    }
+    if password.is_some() {
+        config.insert("password", password.unwrap());
+    }
     config_protocol(config).await?;
     Ok(())
 }
